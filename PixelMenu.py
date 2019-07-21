b9fcdbf6b4de6cd474720d3e7a98e01c33583214
DankCoder <43471842+RoConverse@users.noreply.github.com>
2019-07-21 19:38:13 +0530

    Update PixelMenu.py

--- PixelMenu.py
+++ PixelMenu.py
@@ -19,3 +19,22 @@ def ChangePixels(im, col, col2, out="out.png", tolerance=10, save=True):
             return False
     else:
         return newim
+def DeletePixels(im, col, out="out.png", tolerance=10, save=True):
+    newimdata = []
+    blackcolor = col
+    for color in tuple(im.getdata()):
+        if sum(tuple(color)) in range(sum(blackcolor), sum(blackcolor)+tolerance) or sum(tuple(color)) in range(sum(blackcolor)-tolerance, sum(blackcolor)):
+            newimdata.append((255,255,255,0))
+        else:
+            newimdata.append(tuple(color))
+    newim = Image.new(im.mode,im.size)
+    newim.putdata(tuple(newimdata))
+    if save:
+        try:
+            newim.save(out)
+            return True
+        except Exception as ex:
+            print(ex)
+            return False
+    else:
+        return newim
